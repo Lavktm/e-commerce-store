8e6db894d8dced39713468a85c58a8ccacb24f45
lavktm <lav.ktm@gmail.com>
2023-09-14 17:30:42 +0530

    Billboard addeed

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,23 @@
-const HomePage = () => {
+import getBillboards from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container"
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true })
+    const billboards = await getBillboards("63b0bed2-aa55-42ab-b8d6-312bba70e323");
     return (
-        <div>
-            Hello Store
-        </div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboards} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
+            </div>
+        </Container>
     )
 }
 
--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
 export interface Billboard {
     id: string,
-    name: string,
+    label: string,
     imageUrl: String,
 }
 
@@ -8,4 +8,32 @@ export interface Category {
     id: String,
     name: String,
     billboard: Billboard
+}
+
+export interface Product {
+    id: string;
+    category: Category;
+    name: string;
+    price: string;
+    isFeatured: boolean;
+    size: Size;
+    color: Color;
+    images: Image[];
+}
+
+export interface Image {
+    id: string;
+    url: string
+}
+
+export interface Size {
+    id: string;
+    name: string;
+    value: string;
+}
+
+export interface Color {
+    id: string;
+    name: string;
+    value: string;
 }
